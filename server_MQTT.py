00426097b097e0a0d346e304817248d49920841b
Marco Milos <A01424280@tec.mx>
2023-05-11 18:04:19 -0600

    add function on_message

--- server_MQTT.py
+++ server_MQTT.py
@@ -1,3 +1,12 @@
 import paho.mqtt.client as mqtt #import the client1
 import time
 
+#############
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+    
+################################################################
+
